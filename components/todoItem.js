bf0e291bf015ed3e92952d2744f15d1fcc3f6f96
Bastian Álvarez <reyesalvarezab@gmail.com>
2022-12-17 16:33:18 -0300

    Actualización todoItem y todo.json

--- components/todoItem.js
+++ components/todoItem.js
@@ -2,6 +2,7 @@ import { CLIENT_STATIC_FILES_RUNTIME_REACT_REFRESH } from "next/dist/shared/lib/
 import { useState, useEffect } from "react";
 
 var precio = 0;
+var cant = 0;
 
 const TodoItem = ({ item }) => {
   const [checked, setChecked] = useState(item.done);
@@ -15,16 +16,75 @@ const TodoItem = ({ item }) => {
     localStorage.setItem(item.id, JSON.stringify(checked));
   }, [checked]);
 
+  function actualizarListaPedido(num, suma){
+    let pedido = JSON.parse(localStorage.getItem("pedido"));
+    console.log(pedido.length)
+    console.log(pedido);
+    for (let i = 0; i < pedido.length; i++){
+      if (pedido[i][0] == num){
+        if (pedido[i][1]>0){
+        pedido[i][1] += suma;
+        }
+      }
+    }
+    /*let i = {}
+    for (i in pedido){
+      console.log(i)
+      if (i[pedido.id] == num){
+        i[pedido.cantidad] += suma;
+      }
+      else {}
+    }
+    */
+
+                                   //Como obtener un elemento de una de las estructuras del arreglo
+    /*for (var i=0; i<pedido.lenght; i++){
+      if (pedido[i].id == num){
+        pedido[i].cantidad += suma;
+        localStorage.setItem("pedido", JSON.stringify(pedido));
+        console.log(pedido)
+      }
+      else{
+        crearListaPedido(num);
+      }
+  }*/
+    
+
+  }
+
+  function crearListaPedido(identificador){
+    let newPedido =[identificador, 1]
+    let pedido = [newPedido];
+    console.log(pedido)
+    localStorage.setItem("pedido", JSON.stringify(pedido));
+    
+  }
+  
+  function getPedido(suma){
+    if (localStorage.getItem("pedido")){
+      console.log("Actualizando pedido...")
+      actualizarListaPedido(item.id, suma);
+    }
+    else{
+      console.log("Creando nuevo pedido...")
+      crearListaPedido(item.id)
+    }
+    
+  }
+
   function guardar_total_localstorage(suma){
     let total = JSON.parse(localStorage.getItem("total"));
     total = total + suma;
+    if (total<0){
+      total -= suma;
+    }
     localStorage.setItem("total", JSON.stringify(total));
     console.log(total);
   }
 
   function cargar_total_localstorage(){
     if (localStorage.getItem("total")){
-      let total = JSON.parse(localStorage.getItem( " total" ));
+      let total = JSON.parse(localStorage.getItem( "total" ));
     }
     else{
       guardar_total_localstorage(0);
@@ -40,15 +100,10 @@ const TodoItem = ({ item }) => {
     b.innerHTML="Total $"+a;
   }
 
-  function sum(k){
-    if(k)
-      precio += item.price;
-
-    else
-      precio -= item.price;
-
+  function actualizarInfoItem(k,suma){
+    getPedido(suma);
     cargar_total_localstorage();
-    guardar_total_localstorage(precio)
+    guardar_total_localstorage(k)
     refresh();
   }
 
@@ -60,22 +115,18 @@ const TodoItem = ({ item }) => {
         
         <button id="botoncito" onClick={(e) =>{
             precio = item.price;        
-            cargar_total_localstorage();
-            guardar_total_localstorage(precio)
-            refresh();
+            actualizarInfoItem(precio,1);
         }
         }> +
         </button>
         /
         <button onClick={(e) =>{
             precio = -item.price;        
-            cargar_total_localstorage();
-            guardar_total_localstorage(precio)
-            refresh();
+            actualizarInfoItem(precio,-1);
         }
         }> -
         </button>
-
+        <div className="cantidad-item">{item.cantidad}</div>
       </div>
     </>
   );
