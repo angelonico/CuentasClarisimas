160f25d7d4b527bc5e5595981c8c8ec9297d8de0
Bastian Álvarez <reyesalvarezab@gmail.com>
2022-12-17 16:15:01 -0300

    Creación del arreglo de un pedido como localStorage

---
+++ pages/mesa.js
@@ -0,0 +1,61 @@
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../components/layout";
+import TodoItem from "../components/todoItem";
+
+
+import { useEffect } from "react";
+import { measureMemory } from "vm";
+
+export async function getServerSideProps(context) {
+  const res = await fetch("http://localhost:3000/data/todo.json");
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: { data },
+  };
+}
+
+
+const Index = ({data}) => {
+  useEffect(() => {
+    data.map((item, index) => {
+      console.log(item.id);
+      if (localStorage.getItem(item.id) === null) {
+        localStorage.setItem(item.id, JSON.stringify(item.done));
+      }
+      //localStorage.setItem(item.id + "__object", JSON.stringify(item));
+    });
+  }, []);
+  
+  return (
+<Layout pageId="page4">
+    <div>
+      <Head>
+        <title>Menú</title>
+      </Head>
+      <h1 id = "carta">Carta disponible</h1>
+      <div className="todo-list">
+        {data.map((item, index) => (
+          <TodoItem key={index} item={item} />
+        ))}
+      </div>
+            <div id = "total_a_pagar"></div>
+            <button type="button"> Agregar </button>
+    </div>
+    <footer>
+      <a className="card" href="/..">Volver</a>
+    </footer>
+
+</Layout>
+  
+  );
+};
+
+export default Index;
