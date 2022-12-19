26f67844d4162ef3b4fc4023afa18d61cd0233c8
angelo <anbtbarrientos11@gmail.com>
2022-12-19 19:21:05 -0300

    Carga de valores (total y cantidad) mediante un useEffect

--- components/todoItem.js
+++ components/todoItem.js
@@ -1,38 +1,53 @@
 import { CLIENT_STATIC_FILES_RUNTIME_REACT_REFRESH } from "next/dist/shared/lib/constants";
 import { useState, useEffect } from "react";
+import { isConditionalExpression } from "typescript";
 
 var precio = 0;
 var cant = 0;
 
 const TodoItem = ({ item }) => {
   const [checked, setChecked] = useState(item.done);
-
+  
   useEffect(() => {
     if (localStorage.getItem(item.id) !== null)
-      setChecked(JSON.parse(localStorage.getItem(item.id)));
+    setChecked(JSON.parse(localStorage.getItem(item.id)));
   }, []);
-
+  
   useEffect(() => {
     localStorage.setItem(item.id, JSON.stringify(checked));
   }, [checked]);
-
+  
+  useEffect(()=>{
+    refresh();
+    actualizarInfoItem(0,0);
+  });
+    
+  
   function actualizarListaPedido(id, cantidad){ //id es el id del producto, y cantidad es la cantidad de productos que se están solicitando, puede ser 1 (pedir) o -1 (devolver)
     let pedido = JSON.parse(localStorage.getItem("pedido")); // "Recoge" el pedido guardado en el localStorage
     var pedido_existente = false; // Booleano que se usa para ver si el cliente ya pidió el mismo elemento anteriormente
-    let x = false; // Booleano utilizado para verificar si se realizo algún pedido y se haya modificado el arreglo del pedido del cliente (ya seaa agregar o quitar elementos) 
+    let itemCantidad=document.querySelectorAll(".cantidad-item");
+    let x = false; // Booleano utilizado para verificar si se realizo algún pedido y se haya modificado el arreglo del pedido del cliente (ya seaa agregar o quitar elementos)
     for (let i = 0; i < pedido.length; i++){ //Recorrer el pedido de 1 cliente
       if (pedido[i][0] == id){ // verifica si el elemento ya fue pedido anteriormente
         pedido_existente = true;
         if ((pedido[i][1] + cantidad)>=0){  // verifica que no se devuelvan elementos que no se hayan pedido (imposibilita tener -1 elementos)
           pedido[i][1] += cantidad;
           x = true;
+          itemCantidad[id].innerHTML=pedido[i][1];
+          let n=0;
+          for(let j in itemCantidad){
+            console.log("Item "+n+" "+j);
+            n++;
+          }
         }
       }
     }
     if (!pedido_existente){ //Si el elemento que se esta pidiendo no se pidió con anterioridad, entra aquí
       if (cantidad>0){ //Verifica que no se estén restando elementos
-      var agregar_pedido = [id,cantidad];
-      pedido.push(agregar_pedido);
+        var agregar_pedido = [id,cantidad];
+        pedido.push(agregar_pedido);
+        itemCantidad[id].innerHTML=pedido[pedido.length-1][1];
       x = true;
       }
     }
@@ -42,7 +57,6 @@ const TodoItem = ({ item }) => {
 
   function crearListaPedido(id){
     let pedido = [[id, 1]];
-    console.log(pedido)
     localStorage.setItem("pedido", JSON.stringify(pedido));
     return true;
     
@@ -94,10 +108,11 @@ const TodoItem = ({ item }) => {
     if (getPedido(cant)){
     cargar_total_localstorage();
     guardar_total_localstorage(precio);
-  }
+    }
     refresh();
   }
 
+  
   return (
     <>
       <div className="todo-item">
@@ -121,5 +136,6 @@ const TodoItem = ({ item }) => {
       </div>
     </>
   );
+  
 };
 export default TodoItem;
